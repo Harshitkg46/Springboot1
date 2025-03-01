a808bf6e1adc507be5658322e18adb9374ea5031
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-01 15:53:34 +0530

    Updated UC2

--- springbootconcepts/springbootconcepts/src/main/java/com/example/springbootconcepts/HelloController.java
+++ springbootconcepts/springbootconcepts/src/main/java/com/example/springbootconcepts/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springbootconcepts;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,5 +12,15 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+}
+
+@RestController
+class HelloControllerParam {
+
+    // Handle GET request with query parameter
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
