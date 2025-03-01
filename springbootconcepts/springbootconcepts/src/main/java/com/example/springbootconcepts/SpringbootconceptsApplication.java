b46c7994a890f271aa583c596378eb7fc24551b6
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-01 15:48:20 +0530

    UC1 Updated

---
+++ springbootconcepts/springbootconcepts/src/main/java/com/example/springbootconcepts/SpringbootconceptsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootconceptsApplication.class, args);
+		System.out.println("Spring Boot Application Started Successfully!");
+	}
+}
