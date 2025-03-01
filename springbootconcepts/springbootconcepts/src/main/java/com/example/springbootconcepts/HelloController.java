209705644f3af2843984036644da09402127968c
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-01 15:58:55 +0530

    UC3

--- springbootconcepts/springbootconcepts/src/main/java/com/example/springbootconcepts/HelloController.java
+++ springbootconcepts/springbootconcepts/src/main/java/com/example/springbootconcepts/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/hello")
@@ -21,6 +22,16 @@ class HelloControllerParam {
     // Handle GET request with query parameter
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
+
+@RestController
+class HelloControllermark {
+
+    // Handle GET request with path variable
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloWithPathVariable(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
